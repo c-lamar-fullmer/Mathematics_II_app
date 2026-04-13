b16658f0dfb832c18ef248e8e0bd52d4d7cbccda
c-lamar-fullmer <colton.lamar.fullmer@gmail.com>
2026-04-13 15:16:12 +0200

    Get app to deploy on Vercel

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Shows ALL questions on the home page
     return render_template('index.html', questions=questions_data)
 
 @app.route('/unit/<int:unit_id>')
 def view_unit(unit_id):
-    # This creates a new list containing ONLY questions for the clicked unit
     filtered_questions = [q for q in questions_data if q['unit'] == unit_id]
     return render_template('index.html', questions=filtered_questions, current_unit=unit_id)
 
+# Vercel will look for the 'app' variable defined above.
+# We keep the block below so you can still test locally with 'poetry run python app.py'
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
